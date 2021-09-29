4d0be46ebcf249a52d5ac3926ebfcaab268637fe
Robert Premar <robert.premar@gmail.com>
2021-09-29 15:04:19 +0200

    Add abbility to increment bill identifier

--- src/Bill/Identifier.php
+++ src/Bill/Identifier.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Robier\Fiscalization\Bill;
 
+use Robier\Fiscalization\Exception\InvalidArgument;
+
 final class Identifier
 {
     public function __construct(
@@ -27,5 +29,29 @@ final class Identifier
     public function tollDeviceNumber(): int
     {
         return $this->tollDeviceNumber;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf(
+            '%d/%s/%d',
+            $this->billNumber,
+            $this->storeDesignation,
+            $this->tollDeviceNumber
+        );
+    }
+
+    public static function fromString(string $identifier): self
+    {
+        if (preg_match('/(\d+)\/(\w+)\/(\d+)/i', $identifier, $result) === 0) {
+            throw new InvalidArgument('Provided identifier is not in good format');
+        }
+
+        return new self((int)$result[1], $result[2], (int)$result[3]);
+    }
+
+    public function next(): self
+    {
+        return new static($this->billNumber + 1, $this->storeDesignation, $this->tollDeviceNumber);
     }
 }
